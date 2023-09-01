9ee75cdfdb1bad93dd621783c489561c7edcee7a
barez azad <barez.001159544@gmail.com>
2023-09-01 21:11:44 +0300

    Modified

--- lib/login.dart
+++ lib/login.dart
@@ -6,9 +6,6 @@ import 'package:bn_sl/widgets/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'models/response_login.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -16,26 +13,22 @@ class Login extends StatefulWidget {
   @override
   State<Login> createState() => _LoginState();
 }
-String error="";
+
+String error = "";
 bool _obscureText = true;
 String username = "";
 String password = "";
 Color loginBtn = btnColor;
 
+TextEditingController _emailController = TextEditingController();
+TextEditingController _passwordController = TextEditingController();
+
 class _LoginState extends State<Login> {
-  bool _isChecked = false;
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
   @override
   void initState() {
     super.initState();
-    // _loadUserEmailPassword();
   }
 
-  // final Future<String> _calculation = Future<String>.delayed(
-  //   const Duration(seconds: 2),
-  //   () => 'Data Loaded',
-  // );
   @override
   Widget build(BuildContext context) {
     double wid = MediaQuery.of(context).size.width;
@@ -52,12 +45,14 @@ class _LoginState extends State<Login> {
         body: SafeArea(
           child: Center(
             child: Container(
-              width: wid>widthResponsiveness?wid/2:wid,
-              height: wid>widthResponsiveness?hei/1.7:hei,
+              width: wid > widthResponsiveness ? wid / 2 : wid,
+              height: wid > widthResponsiveness ? hei / 1.7 : hei,
               decoration: BoxDecoration(
-                borderRadius: wid>widthResponsiveness?BorderRadius.all(
-                  Radius.circular(35),
-                ):null,
+                borderRadius: wid > widthResponsiveness
+                    ? BorderRadius.all(
+                        Radius.circular(35),
+                      )
+                    : null,
                 color: bColor,
                 boxShadow: [
                   BoxShadow(
@@ -93,17 +88,14 @@ class _LoginState extends State<Login> {
                     child: Column(
                       children: [
                         loginFields(
-                            wid,80, "Username", Icons.person_outline_rounded,
+                            wid, 80, "Username", Icons.person_outline_rounded,
                             (val) {
                           setState(() {
                             username = val;
                           });
                         }, () {
-                          setState(() {
-                            // _obscureText==true?_obscureText=false:_obscureText=true;
-                          });
+                          //
                         }, false, _emailController),
-                        // loginFields(wid, "Username", Icons.person_outline_rounded),
                         SizedBox(
                           height: 15,
                         ),
@@ -114,12 +106,9 @@ class _LoginState extends State<Login> {
                           });
                         }, () {
                           setState(() {
-                            _obscureText == true
-                                ? _obscureText = false
-                                : _obscureText = true;
+                            _obscureText == true ? _obscureText = false : _obscureText = true;
                           });
                         }, _obscureText, _passwordController),
-                        // loginFields(wid, "Password", Icons.lock_outline_rounded),
                       ],
                     ),
                   ),
@@ -128,127 +117,44 @@ class _LoginState extends State<Login> {
                   ),
                   Align(
                     alignment: Alignment.centerLeft,
-                      child: Text("$error", style: TextStyle(color: Colors.red, fontSize: 10),),),
-                  SizedBox(
-                    height: 30,
+                    child: Text(
+                      "$error",
+                      style: TextStyle(color: Colors.red, fontSize: 10),
+                    ),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 40,
                   ),
                   MouseRegion(
                     onHover: (a) {
                       setState(() {
                         loginBtn = btnColorHover;
                       });
-                      print("Hovered!");
                     },
                     onExit: (a) {
                       setState(() {
                         loginBtn = btnColor;
                       });
-                      print("Hover Out");
                     },
                     child: InkWell(
                       onTap: () async {
-                        if(_emailController.text=="" || _passwordController.text==""){
-                          error="Fill in username and password";
-                        }else{
-                          var requstd = new LoginRequest(
-                              _emailController.text, _passwordController.text);
-                          // // requstd.username=_emailController.text;
-                          // // requstd.password=_passwordController.text;
-                          // //
-                          // // print("${requstd.username}  and m ${requstd.password}");
-                          // DataServices services = DataServices(requstd.username, requstd.password);
-                          // String token;
-                          // token = await services.login();
-                          // // services.getLogin(_emailController.text, _passwordController.text);
-                          // // print(token);
-
-                          print(username);
-                          print(password);
+                        if (_emailController.text == "" || _passwordController.text == "") {
+                          error = "Fill in username and password";
+                        } else {
+                          var requstd = new LoginRequest(_emailController.text, _passwordController.text);
                           UserManager uM = UserManager();
-                          var res= await uM.login(requstd);
-                          print("$res))))))))))");
-                          if(res != null){
-                            Navigator.push(context, MaterialPageRoute(builder: (ctx){
+                          var res = await uM.login(requstd);
+                          if (res != null) {
+                            Navigator.push(context, MaterialPageRoute(builder: (ctx) {
                               return UserDetails();
                             }));
-                          }else{
+                          } else {
                             setState(() {
-                              error="Username or password invalid";
+                              error = "Username or password invalid";
                             });
-                            //error display
                           }
                         }
-                        // Future<String> token = await returnedToken();
-    //                     final Future<String> _calculationn =
-    //                         Future<String>.delayed(
-    //                       const Duration(seconds: 2),
-    //                       () async{
-    //                         final storage = new FlutterSecureStorage();
-    //                         String? token=await storage.read(key: "accessToken");
-    //                         return token!;
-    //                         // return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM4M2Q3NDFjLTkyMTEtNDAwZi1hNmQyLTQ4NjU4MzZmNTFmZSIsIndhcmVob3VzZWlkIjoiMSIsImlzcmVwcmVzZW50YXRpdmUiOiJGYWxzZSIsImZ1bGxuYW1lIjoiaGltZGFkIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoiaGltZGFkQHR3YW4uY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImhpbWRhZCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJTdG9jayIsIkRhc2hib2FyZCIsIk1hbmFnZXIiLCJSZXBvcnQiXSwibmJmIjoxNjg5MDA1ODQyLCJleHAiOjE2OTAyMTU0NDIsImlzcyI6Imh0dHA6Ly9iYXJlemF6YWQtMDAxLXNpdGUxLmN0ZW1wdXJsLmNvbS8iLCJhdWQiOiJodHRwOi8vYmFyZXphemFkLTAwMS1zaXRlMS5jdGVtcHVybC5jb20ifQ.tk3SXr0CvTlaRbHEP9Ah8KTnxftzM3v7Fyp8h9FEnZ8";
-    //                       },
-    //                     );
-    //                     Navigator.push(context,
-    //                         MaterialPageRoute(builder: (ctx) {
-    //                       // returnedToken();
-    //                       // return UserDetails(token);
-    //                        return FutureBuilder(
-    //                           future: _calculationn,
-    //                           builder: (BuildContext context,
-    //                               AsyncSnapshot<String> snapshot) {
-    //                             if (snapshot.hasData) {
-    //                               return UserDetails(snapshot.data.toString());
-    //                               // print("***************************l");
-    //                               // print(snapshot);
-    //                               // UserDetails(snapshot.data.toString());
-    //                               // print("***************************l");
-    //                               // UserDetails(snapshot.data.toString());
-    //                             } else if (snapshot.hasError) {
-    //                               Column(
-    //                                   children : <Widget>[
-    //                                     const Icon(
-    //                                       Icons.error_outline,
-    //                                       color: Colors.red,
-    //                                       size: 60,
-    //                                     ),
-    //                                     Padding(
-    //                                       padding: const EdgeInsets.only(top: 16),
-    //                                       child: Text('Error: ${snapshot.error}'),
-    //                                     ),
-    //                                   ],
-    //                               );
-    //                             }else{
-    //                               Column(
-    //                                   children: const <Widget>[
-    //                                     SizedBox(
-    //                                       width: 60,
-    //                                       height: 60,
-    //                                       child: CircularProgressIndicator(),
-    //                                     ),
-    //                                     Padding(
-    //                                       padding: EdgeInsets.only(top: 16),
-    //                                       child: Text('Awaiting result...'),
-    //                                     ),
-    //                                   ],
-    //                               );
-    //                             }
-    //                             print("***************************l");
-    //                             print(snapshot.data);
-    //                             print("***************************l");
-    //                             return UserDetails(snapshot.data.toString());
-    //                           });
-    // }));
                       },
-                      // onHover: (val){
-                      //   setState(() {
-                      //     val == true ? loginBtn = btnColorHover: loginBtn = btnColor;
-                      //   });
-                      //   print("HOVERED");
-                      // },
                       child: Container(
                         width: wid * 0.9,
                         height: 60,
@@ -270,29 +176,6 @@ class _LoginState extends State<Login> {
                       ),
                     ),
                   ),
-                  // SizedBox(
-                  //   height: 30,
-                  // ),
-                  // Row(
-                  //   mainAxisAlignment: MainAxisAlignment.center,
-                  //   children: [
-                  //     Text("Don't have an account? ", style: TextStyle(color: Colors.black54),),
-                  //     GestureDetector(
-                  //       onTap: () {
-                  //         setState(() {
-                  //           print("Sign Up");
-                  //         });
-                  //       },
-                  //       child: Container(
-                  //         child: Text(
-                  //           "Sign Up",
-                  //           style: TextStyle(
-                  //               color: btnColor, fontWeight: FontWeight.bold),
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
                 ],
               ),
             ),
@@ -301,71 +184,4 @@ class _LoginState extends State<Login> {
       ),
     );
   }
-
-  // void _handleRemeberme(bool value) {
-  //   _isChecked = value;
-  //   SharedPreferences.getInstance().then(
-  //     (prefs) {
-  //       prefs.setBool("remember_me", value);
-  //       prefs.setString('email', _emailController.text);
-  //       prefs.setString('password', _passwordController.text);
-  //     },
-  //   );
-  //   setState(() {
-  //     _isChecked = value;
-  //   });
-  // }
-
-  // void _handleRemebermee(bool value) {
-  //   print("Handle Rember Me");
-  //   _isChecked = value;
-  //   SharedPreferences.getInstance().then(
-  //     (prefs) {
-  //       prefs.setBool("remember_me", value);
-  //       prefs.setString('email', _emailController.text);
-  //       prefs.setString('password', _passwordController.text);
-  //     },
-  //   );
-  //   setState(() {
-  //     _isChecked = value;
-  //   });
-  // }
-
-  Future<String?> returnedToken() async {
-    String? token = "";
-    // try {
-    final storage = new FlutterSecureStorage();
-    token = await storage.read(key: "accessToken");
-    // } catch (e) {
-    //   print(e);
-    // }
-    print("========================+");
-    print(token);
-    print("========================+");
-    return token;
-  }
-
-  // void _loadUserEmailPassword() async {
-  //   print("Load Email");
-  //   try {
-  //     SharedPreferences _prefs = await SharedPreferences.getInstance();
-  //     var _email = _prefs.getString("email") ?? "";
-  //     var _password = _prefs.getString("password") ?? "";
-  //     var _remeberMe = _prefs.getBool("remember_me") ?? false;
-  //
-  //     print(_remeberMe);
-  //     print(_email);
-  //     print(_password);
-  //     if (_remeberMe) {
-  //       setState(() {
-  //         _isChecked = true;
-  //       });
-  //       _emailController.text = _email ?? "";
-  //       _passwordController.text = _password ?? "";
-  //     }
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  // }
-
 }
